94d2f8ed53260fae32f7b0648c4afc7273294c44
schance14 <schance.sd14@gmail.com>
2023-06-04 12:38:05 -0600

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+let pokemonListArray = [
+    { name: 'Snorlax', height: 2.1, types: 'normal'},
+    { name: 'Raichu', height: .8, types: 'electric'},
+    { name: 'Ponyta', height: 1, types: 'fire'}
+];
