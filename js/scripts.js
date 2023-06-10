7ddfbf54460c0f8e6255f298510b635d1202c321
schance14 <schance.sd14@gmail.com>
2023-06-09 18:38:56 -0600

    new loop
    New loop to display pokemon list to webpage

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,11 @@
-let pokemonListArray = [
+let pokemonList = [
     { name: 'Snorlax', height: 2.1, types: 'normal'},
     { name: 'Raichu', height: .8, types: 'electric'},
     { name: 'Ponyta', height: 1, types: 'fire'}
 ];
-
-
-let personAge = [
-    {name: 'person1', age: 16},
-    {name:'person2', age: 10},
-    {name: 'person3', age: 25}
-];
-
-for (let i=0; i < personAge.length; i++){
-    if (personAge[i].age <19 && personAge[i].age >13){
-        console.log(personAge[i].name + ' is a teenager');
-    } else if (personAge[i].age <13){
-        console.log(personAge[i].name + ' is a child');
-} else { 
-    console.log(personAge[i].name + 'is an adult');
+for (let i=0;i <pokemonList.length; i++ ){
+    if (pokemonList[i].height<1.1)
+    console.log(pokemonList[i].name + pokemonList.height + 'That is one big sleepy boy!');
+}else if(pokemonList[i].height>1){
+    console.log(pokemonList.name + pokemonList.height);
 }
-} 
