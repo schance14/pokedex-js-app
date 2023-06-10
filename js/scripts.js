5d6c09cb43df34540e4d1a3ad93b151289765861
schance14 <schance.sd14@gmail.com>
2023-06-09 19:18:13 -0600

    updated more notation errors

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,12 @@
 let pokemonList = [
-    { name: 'Snorlax', height: 2.1, types: 'normal'},
-    { name: 'Raichu', height: .8, types: 'electric'},
-    { name: 'Ponyta', height: 1, types: 'fire'}
+    { name: 'Snorlax', height: 2.1, type: 'normal'},
+    { name: 'Raichu', height: .8, type: 'electric'},
+    { name: 'Ponyta', height: 1, type: 'fire'}
 ];
 for (let i=0;i <pokemonList.length; i++ ){
-    if (pokemonList[i].height<1.1){
+    if (pokemonList[i].height>1.1){
     document.write(pokemonList[i].name + pokemonList[i].height + ' That is one big sleepy boy!'+ "<br/>");
-}else if(pokemonList[i].height>1){
-    document.write(pokemonList.name + pokemonList[i].height + " <br/>");
+}else if(pokemonList[i].height<1.1){
+    document.write(pokemonList[i].name + pokemonList[i].height + " <br/>");
 }
 }
