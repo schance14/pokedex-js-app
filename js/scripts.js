adf994426fdde2bf454cf00a6cd8d6dea5698c34
schance14 <schance.sd14@gmail.com>
2023-06-22 19:10:18 -0600

    Updated loop code and added IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,38 @@
-let pokemonList = [
+let pokemonRepository = (function () {
+
+    let pokemonList = [
     { name: 'Snorlax', height: 2.1, type: 'normal'},
     { name: 'Raichu', height: .8, type: 'electric'},
     { name: 'Ponyta', height: 1, type: 'fire'}
 ];
-for (let i=0;i <pokemonList.length; i++ ){
+
+function getAll() {
+    return pokemonList;
+}
+function add (pokemon) {
+    pokemonList.push(pokemon);
+}
+
+return {
+    getAll: getAll,
+    add: add
+};
+})();
+
+document.write(pokemonRepository.getAll());
+
+function myLoopFunction(pokemon) {
+    document.write(pokemon.name + ' is ' + pokemon.height + ' mm, and their pokemon type is ' + pokemon.type + " <br/>");
+};
+
+
+pokemonList.forEach(myLoopFunction);
+
+
+/*for (let i=0;i <pokemonList.length; i++ ){
     if (pokemonList[i].height>1.1){
     document.write(pokemonList[i].name + '&nbsp;' + pokemonList[i].height + ' That is one big sleepy boy!'+ '<br/>');
 }else if(pokemonList[i].height<1.1){
     document.write(pokemonList[i].name + '&nbsp;' + pokemonList[i].height + " <br/>");
 }
-}
+}*/
