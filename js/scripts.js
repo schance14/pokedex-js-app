85235431d3f949c7599fda901b4810088ef2a192
schance14 <schance.sd14@gmail.com>
2023-07-15 13:43:10 -0600

    added functions to JS

--- js/scripts.js
+++ js/scripts.js
@@ -6,27 +6,62 @@ let pokemonRepository = (function () {
     { name: 'Ponyta', height: 1, type: 'fire'}
 ];
 
-function getAll() {
-    return pokemonList;
-}
-function add (pokemon) {
-    pokemonList.push(pokemon);
+function add(pokemon) {
+    if (
+      typeof pokemon === "object" &&
+      "name" in pokemon &&
+      "height" in pokemon &&
+      "types" in pokemon
+    ) {
+      pokemon.push(pokemon);
+    } else {
+      console.log("pokemon is not correct");
+    }
+  }
+  function getAll() {
+    return pokemon;
+  }
+function addListItem(pokemon){
+    let pokemonList = document.querySelector('.pokemon-list');
+    let listpokemon = document.createElement('li');
+    let button = document.createElement('button');
+    button.innerText = pokemon.name;
+    button.classList.add('button-class');
+    listpokemon.appendChild(button);
+    pokemonList.appendChild(listPokemon);
+
+    button.addEventListener("click", function () {
+      showDetails(pokemon);
+    });
+
 }
 
+function showDetails(pokemon){
+  const button = document.createElement('button');
+  console.log()
+
+}
 return {
     getAll: getAll,
-    add: add
+    add: add,
+    addListItem: addListItem, 
+    showDetails: showDetails
 }
 })();
 
 pokemonRepository.getAll().forEach(function (pokemon) {
-document.write(
-pokemon.name + " is " + pokemon.height + " mm, and their pokemon type is " + pokemon.type + "</br>"
-);
+    pokemonRepository.addListItem(pokemon);
 });
 
 
+let message;
+message= "Here is a list of my favorite pokemon, enjoy!!";
+
+alert(message);
+
 pokemonList.forEach(myLoopFunction); 
+
+
 
 
 /* previous code, saving as a ref..
@@ -37,3 +72,14 @@ for (let i=0;i <pokemonList.length; i++ ){
     document.write(pokemonList[i].name + '&nbsp;' + pokemonList[i].height + " <br/>");
 }
 }*/
+
+
+/*pokemonRepository.getAll().forEach(function (pokemon) {
+    let pokemonList = document.querySelector('.pokemon-list');
+    let listpokemon = document.createElement('li');
+    let button = document.createElement('button');
+    button.innerText = pokemon.name;
+    button.classList.add('button-class');
+    listpokemon.appendChild(button);
+    pokemonList.appendChild(listPokemon);
+});*/
